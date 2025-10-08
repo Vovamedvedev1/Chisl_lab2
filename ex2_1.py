c0cfb285592324726320e06b2fc12e12c6a7c962
medve <vladim1r.medv@yandex.ru>
2025-10-08 09:59:43 +0700

    111111111111111111111111111111111111

---
+++ ex2_1.py
@@ -0,0 +1,51 @@
+import numpy as np
+from math import sqrt
+from lagrange_polinom import function, get_max_derivative
+
+def get_gauss_polinom(x_0, function_str, a, b, eps):
+    h = (b - a)
+    R = float('inf')  
+    max_iter = 10000  
+    count_iter = 0
+    while abs(R) > eps and count_iter <= max_iter:
+        h /= 2
+        x = np.arange(a, b + h, h)
+        n = len(x)
+        y = function(x, math_value=function_str)
+        imin = 0
+        d_min = abs(x[0] - x_0)
+        for i in range(1, n):
+            d = abs(x[i] - x_0)
+            if d < d_min:
+                imin, d_min = i, d
+        if imin == 0:
+            imin = 1
+        if imin == n-1:
+            imin = n - 2
+        y_i = y[imin]
+        y_i_1 = y[imin + 1] - y[imin]
+        y_i_2 = y[imin - 1] - 2 * y[imin] + y[imin + 1]
+        t = (x_0 - x[imin]) / h
+        L = y_i + y_i_1 * t + y_i_2 * t * (t - 1) / 2
+        R = abs(get_max_derivative(function_str, 3, x[imin-1], x[imin+1]) *(h ** 3) * t * (t * t - 1) / 6)
+        print(f"Итерация: {count_iter}; h = {h}; L = {L}; R = {R}")
+        count_iter += 1
+    return L
+
+try:
+    func_string = input("Введите функцию: ")
+    a = float(input("Введите a: "))
+    b = float(input("Введите b: "))
+    if a >= b:
+        raise Exception("а должно быть строго меньше b")
+    x_0 = float(input("Введите x0: "))
+    if x_0 < a or x_0 > b:
+        raise Exception("x_0 не должно выходить за пределы указанного диапазона")
+    teor = function(x_0, math_value=func_string)
+    pract = get_gauss_polinom(x_0, func_string, a, b,eps=10**(-10))
+    print("Теоретическое значение: ", teor)
+    print("Вычиссленное значение: ", pract)
+    print("Абсолютная погрешность: ", abs(teor - pract))
+    print("Относительная погрешность: ", abs(teor - pract) / abs(pract))
+except Exception as e:
+    print(str(e))
--- ex2_2.py
+++ ex2_2.py
@@ -2,35 +2,23 @@ import numpy as np
 from math import sqrt
 from lagrange_polinom import function, get_max_derivative
 
-
 def get_gauss_polinom(x_0, function_str, a, b, eps):
-    h = (b - a)
-    R = float('inf')   #остаточный член
+    hh = min(abs(x_0 - a), abs(b - x_0))
+    h = 2 * hh
+    R = float('inf')   
     max_iter = 10000   
     count_iter = 0 
-    while abs(R) > eps and count_iter <= max_iter: #до тех пор пока остаточный член не удовлетворит точности 10**(-4)
-        h /= 2 #новый шаг получаем делением пополам 
-        x = np.arange(a, b + h, h) #перестраиваем сетку
-        n = len(x) #кол  узлов
-        y = function(x, math_value=function_str)
-        imin = 0
-        d_min = abs(x[0] - x_0)
-        for i in range(1, n):
-            d = abs(x[i] - x_0)
-            if d < d_min:
-                imin, d_min = i, d
-        if imin == 0:
-            imin = 1
-        if imin == n-1:
-            imin = n - 2
-        y_i = y[imin]
-        y_i_1 = y[imin + 1] - y[imin]
-        y_i_2 = y[imin - 1] - 2 * y[imin] + y[imin + 1]
-        t = (x_0 - x[imin]) / h
+    while abs(R) > eps and count_iter <= max_iter: 
+        x__0, x__1, x__2 = x_0 - h / 2, x_0 + h / 2, x_0 + 3 * h / 2 
+        y_i = function(x__1, math_value=function_str)
+        y_i_1 = function(x__2, math_value=function_str) - function(x__1, math_value=function_str)
+        y_i_2 = function(x__0, math_value=function_str) - 2 * function(x__1, math_value=function_str) + function(x__2, math_value=function_str)
+        t = (x_0 - x__1) / h
         L = y_i + y_i_1 * t + y_i_2 * t * (t - 1) / 2
-        R = abs(get_max_derivative(function_str, 3, x[imin-1], x[imin+1]) *(h ** 3) * t * (t * t - 1) / 6)
+        R = abs(get_max_derivative(function_str, 3, x_0, x__2) *(h ** 3) * t * (t * t - 1) / 6)
         print(f"Итерация: {count_iter}; h = {h}; L = {L}; R = {R}")
         count_iter += 1
+        h = h / 2
     return L
 
 try:
@@ -43,7 +31,7 @@ try:
     if x_0 < a or x_0 > b:
         raise Exception("x_0 не должно выходить за пределы указанного диапазона")
     teor = function(x_0, math_value=func_string)
-    pract = get_gauss_polinom(x_0, func_string, a, b,eps=10**(-4))
+    pract = get_gauss_polinom(x_0, func_string, a, b,eps=10**(-10))
     print("Теоретическое значение: ", teor)
     print("Вычиссленное значение: ", pract)
     print("Абсолютная погрешность: ", abs(teor - pract))
